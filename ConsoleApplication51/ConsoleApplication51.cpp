924fe467d7027abc9f9f2d2e597de3f3a3b01aa4
user <user@C1011>
2022-01-20 09:08:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication51/ConsoleApplication51.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+
+double recur(double N, double y)
+
+{
+    y = y * (N / (N - 1)) * (N / (N + 1));
+
+    if (N > 2) { return recur(N - 2, y); }
+    return y;
+}
+//---------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "rus");
+
+    int N;
+    cout << "Введите четное число: ";
+    cin >> N;
+
+    cout << "y= " << recur(N, 1);
+    return 0;
+}
